6d23b624a4a308e1b5b02c95b8d40a1e5237acaf
NicNic0331 <princessnicolesalazar31@gmail.com>
2024-10-15 19:04:16 +0800

    first commit

---
+++ arrayActivity.js
@@ -0,0 +1,14 @@
+let favSingers = ["Ben&Ben", "Taylor Swift", "TJ Monterde"]; // Array of three favorite singers
+
+// Log the first singer in the array using array index notation.
+console.log(favSingers[0]); // Logs "Ben&Ben"
+
+let favNumbers = [11, 31, 9, 6]; // Array of four favorite numbers
+
+let mixedArr = ["string", ["otherarray"], 123, true]; // Array of different data types.
+
+// Access and log each item in the mixedArr using array index notation.
+console.log(mixedArr[0]); // Logs "string"
+console.log(mixedArr[1]); // Logs ["otherarray"]
+console.log(mixedArr[2]); // Logs 123
+console.log(mixedArr[3]); // Logs true
